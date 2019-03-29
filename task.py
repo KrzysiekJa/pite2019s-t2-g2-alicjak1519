fab38ec40f88421e93b7598c0a701a51b4f8f4fc
alicjak1519 <48221385+alicjak1519@users.noreply.github.com>
2019-03-29 14:29:18 +0100

    Add files via upload

--- task.py
+++ task.py
@@ -1,26 +1,48 @@
-#
-#Banking simulator. Write a code in python that simulates the banking system. 
-#The program should:
-# - be able to create new banks
-# - store client information in banks
-# - allow for cash input and withdrawal
-# - allow for money transfer from client to client
-#If you can think of any other features, you can add them.
-#This code shoud be runnable with 'python kol1.py'.
-#You don't need to use user input, just show me in the script that the structure of your code works.
-#If you have spare time you can implement: Command Line Interface, some kind of data storage, or even multiprocessing.
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
+class Account():
+    def __init__(self):
+        self.name = ""
+        self.bank_balance = 0
+
+    def get_name(self):
+        self.name = input()
+    def get_start_bank_balance():
+        self.bank_balance = input()
+
+    def reaction(self,act):
+        act.how_many()
+        pass
+
+class Input():
+    def __init__(self):
+        self.sum = 0
+    def how_many(self):
+        self.sum = input()
+    def action(self):
+
+
+class Withdrawal():
+    def __init__(self):
+        self.sum = 0
+    def how_many(self):
+        self.sum = input()
+
+class Two_accounts_transfer():
+    def __init__(self):
+        self.sum = 0
+    def how_many(self):
+        self.sum = 0
+    def transfer(self,user_plus,user_minus):
+        user_plus.bank_balance += self.sum
+        user_minus.bank_balance -= self.sum
+
+
+user1 = Account()
+    user1.get_name()
+    user1.get_start_bank_balance()
+
+user2 = Account()
+    user2.get_name()
+    user2.get_start_bank_balance()
+
+while True:
+    
